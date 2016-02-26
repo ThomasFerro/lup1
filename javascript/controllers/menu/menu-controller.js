8946157970ff3a15dec02689ac3880445bfa8ad2
deplanque tony (hackid) <deplanque.tony@gmail.com>
2016-02-26 14:02:16 +0100

    ajout filtre json

--- javascript/controllers/menu/menu-controller.js
+++ javascript/controllers/menu/menu-controller.js
@@ -1,14 +1,20 @@
 (function(){
   angular.module('lup1')
-  .controller('MenuController', function(){
+  .controller('MenuController', [ "$http", function($http){
+    var controller = this;
+    var menuItems = [];
+    $http.get('models/menus/left-menu.json').success(function(data){
+     controller.menuItems = data;
+    });
+
     this.selectedMenuItem = 0;
     this.menuItems = menuItems;
 
-    this.setSelectedMenuItem = function(setItemIndex) {
+    this.setSelectedMenuItem = function(setItemIndex){
       this.selectedMenuItem = setItemIndex;
     };
 
-    this.isSelectedMenuItem = function(checkItemIndex) {
+    this.isSelectedMenuItem = function(checkItemIndex){
       return this.selectedMenuItem === checkItemIndex;
     };
 
@@ -19,46 +25,10 @@
       return this.menuItems[index].img_passive;
     };
 
-    this.isHome = function(item) {
+    this.isHome = function(item){
       return item.name === "home";
     };
-  });
 
-  var menuItems = [
-    {
-      index:0,
-      name:"home",
-      url:"home",
-      img_active:"img/menu/home.png",
-      img_passive:"img/menu/home-black.png"
-    },
-    {
-      index:1,
-      name:"gestion des notes",
-      url:"notes",
-      img_active:"img/menu/note.png",
-      img_passive:"img/menu/note-black.png"
-    },
-    {
-      index:2,
-      name:"gestion des stages",
-      url:"stages",
-      img_active:"img/menu/stage.png",
-      img_passive:"img/menu/stage-black.png"
-    },
-    {
-      index:3,
-      name:"gestion des absences",
-      url:"absences",
-      img_active:"img/menu/absence.png",
-      img_passive:"img/menu/absence-black.png"
-    },
-    {
-      index:4,
-      name:"gestion des paramètres",
-      url:"parametres",
-      img_active:"img/menu/system.png",
-      img_passive:"img/menu/system-black.png"
-    }
-  ];
-})();
+  }]);
+}
+)();
--- menu/js/menu-app.js
+++
@@ -1,7 +0,0 @@
-(function()){
-  var app = angular.module('lup1', [ ]);
-  
-
-
-
-})();
